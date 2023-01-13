c11dc3a674825d1bc445ca74ca8a686a93c79120
kkcodett <89243936+kkcodett@users.noreply.github.com>
2023-01-13 10:45:29 +0900

    초기 커밋.

---
+++ mfcGPrj/CDlgImage.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#define MAX_POINT	100
+// CDlgImage 대화 상자
+
+class CDlgImage : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDlgImage)
+
+public:
+	CDlgImage(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CDlgImage();
+
+	CImage m_image;
+	CWnd* m_pParent;
+	int m_nDataCount = 0;
+	CPoint m_ptData[MAX_POINT];
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CDlgImage1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+
+private:
+	void InitImage();
+	void drawData(CDC* pDC);
+};
+
